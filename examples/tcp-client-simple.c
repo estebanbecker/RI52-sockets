e3085f04aebe964f636415d2382a157b706ce5e3
Esteban Becker <git@beckeresteban.eu>
2024-06-13 10:43:52 +0200

    chore: Update TCP client and server to handle receiving and sending messages

--- examples/tcp-client-simple.c
+++ examples/tcp-client-simple.c
@@ -4,7 +4,8 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
-#include <getopt.h> // Add this line to include the definition of 'optarg'
+#include <getopt.h>
+#include <poll.h>
 
 #define BUFFER_SIZE 1000
 
@@ -47,24 +48,40 @@ int main(int argc, char *argv[]) {
         name[len_name] = '\0';
     }
 
+    struct pollfd fds[2];
+    fds[0].fd = STDIN_FILENO;
+    fds[0].events = POLLIN;
+    fds[1].fd = sock_to_server;
+    fds[1].events = POLLIN;
+
+    printf("Enter your message:");
+    fflush(stdout);
 
     while(1) {
-        printf("Enter your message: ");
-        fflush(stdout);
-        size_t bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
-        buffer[bytes_read] = '\0';
-        //Concat name and buffer
-        char message[BUFFER_SIZE+1];
-        strncpy(message, name, BUFFER_SIZE); // Copy the username to the message
-        strcat(message, ": "); // Add a colon and a space after the username
-        strcat(message, buffer); // Add the message after the username
-        size_t bytes_sent = send(sock_to_server, message, bytes_read+len_name+1, 0);
-
-        size_t bytes_recieved = recv(sock_to_server, buffer, BUFFER_SIZE, 0);
-        if (bytes_recieved > 0) {
-            buffer[bytes_recieved] = '\0';
-            printf("Received: %s\n", buffer);
-            printf("From: %s:%d\n", inet_ntoa(remote_address.sin_addr), ntohs(remote_address.sin_port));
+        poll(fds, 2, -1);
+        if (fds[0].revents & POLLIN) {
+            // Input from the user
+            printf("Enter your message: ");
+            fflush(stdout);
+            size_t bytes_read = read(STDIN_FILENO, buffer, BUFFER_SIZE);
+            buffer[bytes_read] = '\0';
+            //Concat name and buffer
+            char message[BUFFER_SIZE+1];
+            strncpy(message, name, BUFFER_SIZE); // Copy the username to the message
+            strcat(message, ": "); // Add a colon and a space after the username
+            strcat(message, buffer); // Add the message after the username
+            size_t bytes_sent = send(sock_to_server, message, bytes_read+len_name+2, 0);
+        }
+        if (fds[1].revents & POLLIN) {
+            // Message from the server
+            size_t bytes_recieved = recv(sock_to_server, buffer, BUFFER_SIZE, 0);
+            if (bytes_recieved > 0) {
+                buffer[bytes_recieved] = '\0';
+                printf("\nReceived: %s\n", buffer);
+                fflush(stdout);
+                printf("Enter your message: ");
+                fflush(stdout);
+            }
         }
     }
     // TODO: Cleanup
--- examples/tcp-server-simple.c
+++ examples/tcp-server-simple.c
@@ -5,8 +5,25 @@
 #include <sys/socket.h>
 #include <unistd.h>
 #include <getopt.h>
+#include <poll.h>
 
 #define BUFFER_SIZE 1000
+#define MAX_CLIENTS 100
+
+struct pollfd clients[MAX_CLIENTS];
+struct sockaddr_in client_addresses[MAX_CLIENTS];
+int nb_clients = 0;
+
+void broadcast_message(char *message, int exclude) {
+    for (int i = 1; i < nb_clients; i++) {
+        if (clients[i].fd != exclude) {
+            ssize_t bytes_sent = send(clients[i].fd, message, strlen(message) + 1, 0);
+            if (bytes_sent == -1) {
+                perror("send");
+            }
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
     char *hostname = "0.0.0.0";
@@ -38,28 +55,39 @@ int main(int argc, char *argv[]) {
     bind(srv_socket, (struct sockaddr *) &my_address, sizeof(struct sockaddr_in)); // Check for errors
 
     listen(srv_socket, 10);
-    int client_socket = accept(srv_socket, (struct sockaddr *) &client_address, &addr_size); // Check for errors
+    clients[0].fd = srv_socket;
+    clients[0].events = POLLIN;
+    nb_clients++;
 
-    printf("New connection from %s:%d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+    printf("Server started on %s:%d\n", hostname, port);
 
     while(1) {
-        size_t bytes_read = recv(client_socket, buffer, BUFFER_SIZE, 0);
-        if (bytes_read > 0) {
-            buffer[bytes_read] = '\0';
-            printf("Received: %s\n", buffer);
-            printf("From: %s:%d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
-            for(int i = 0; i < BUFFER_SIZE; i++) {
-              if(buffer[i] == '\n' || buffer[i] == EOF) {
-                buffer[i] = '\0';
-                break;
-              }
+        poll(clients, nb_clients, -1);
+        for (int i = 0; i < nb_clients; i++) {
+            if (clients[i].revents & POLLIN) {
+                if (clients[i].fd == srv_socket) {
+                    // New connection
+                    int client_socket = accept(srv_socket, (struct sockaddr *) &client_address, &addr_size);
+                    clients[nb_clients].fd = client_socket;
+                    clients[nb_clients].events = POLLIN;
+                    client_addresses[nb_clients] = client_address;
+                    nb_clients++;
+                    printf("New connection from %s:%d\n", inet_ntoa(client_address.sin_addr), ntohs(client_address.sin_port));
+                } else {
+                    // Message from a client
+                    size_t bytes_read = recv(clients[i].fd, buffer, BUFFER_SIZE, 0);
+                    if (bytes_read > 0) {
+                        buffer[bytes_read] = '\0';
+                        printf("Received: %s\n", buffer);
+                        printf("From: %s:%d\n", inet_ntoa(client_addresses[i].sin_addr), ntohs(client_addresses[i].sin_port));
+                        broadcast_message(buffer, clients[i].fd);
+                    }
+                }
             }
-            strcat(buffer, "");
-            size_t bytes_sent = send(client_socket, buffer, strlen(buffer)+1, 0); // Check for errors
         }
     }
-    // TODO: Cleanup
-    close(client_socket);
-    close(srv_socket);
+    for (int i = 0; i < nb_clients; i++) {
+        close(clients[i].fd);
+    }
     return 0;
 }
